2fcc0114fe077648a10b5e1ea2248715bc31fe71
AngelNJ11 <angelnj733@gmail.com>
2025-06-10 16:08:50 -0500

    Configuración inicial

---
+++ src/test/java/pe/vptd/VamosPorTuDepaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.vptd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamosPorTuDepaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
